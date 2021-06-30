89bf30f67e19250b13eef31b28e97fff86ffd887
magkamon <kamonmagdalena@gmail.com>
2021-06-30 15:33:14 +0200

    added functionality for zero element

--- src/main/java/com/epam/Fibonacci.java
+++ src/main/java/com/epam/Fibonacci.java
@@ -2,9 +2,12 @@ package com.epam;
 
 public class Fibonacci {
 
-    public static int countFibonacciSequence(int elementNumber){
-        if(elementNumber < 0){
+    public static int countFibonacciSequence(int elementNumber) {
+        if (elementNumber < 0) {
             throw new IllegalArgumentException();
+        }
+        if (elementNumber == 0) {
+            return 0;
         }
         return -1;
     }
