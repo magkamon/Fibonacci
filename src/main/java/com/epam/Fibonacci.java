e22cd4f90b0781f8c4f965b31bd2ca1158a4973e
magkamon <kamonmagdalena@gmail.com>
2021-06-30 15:27:56 +0200

    added checking negative input functionality in method

--- src/main/java/com/epam/Fibonacci.java
+++ src/main/java/com/epam/Fibonacci.java
@@ -3,6 +3,9 @@ package com.epam;
 public class Fibonacci {
 
     public static int countFibonacciSequence(int elementNumber){
+        if(elementNumber < 0){
+            throw new IllegalArgumentException();
+        }
         return 0;
     }
 
