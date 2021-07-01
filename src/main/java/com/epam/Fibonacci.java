0ed0c7813fc84a047e5d065f6774236db40b7c87
magkamon <kamonmagdalena@gmail.com>
2021-07-01 17:23:25 +0200

    optymalization of Fibonacci method

--- src/main/java/com/epam/Fibonacci.java
+++ src/main/java/com/epam/Fibonacci.java
@@ -1,5 +1,8 @@
 package com.epam;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Fibonacci {
 
     public static long countFibonacciSequence(int elementNumber) {
@@ -12,6 +15,20 @@ public class Fibonacci {
         if (elementNumber == 1) {
             return 1;
         }
-        return countFibonacciSequence(elementNumber - 1) + countFibonacciSequence(elementNumber - 2);
+        List<Long> fibonacciValues = new ArrayList<>();
+        fibonacciValues.add(0L);
+        fibonacciValues.add(1L);
+        countFibonacciSequence(elementNumber, fibonacciValues);
+        return fibonacciValues.get(fibonacciValues.size()-1);
+//        return countFibonacciSequence(elementNumber - 1) + countFibonacciSequence(elementNumber - 2);
+    }
+
+    private static void countFibonacciSequence(int elementNumber, List<Long> list) {
+        if(elementNumber > 1){
+            countFibonacciSequence((elementNumber-1), list);
+            Long n1 = list.get(list.size()-1);
+            Long n2 = list.get(list.size()-2);
+            list.add(n1 + n2);
+        }
     }
 }
